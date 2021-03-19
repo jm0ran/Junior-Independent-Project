99c52b2395fe110f9889bbd88881aeef60887a6a
Joseph Moran <josephdeargmoran@Gmail.com>
2021-03-19 10:34:00 -0400

    Very Basic Form Validation

--- routing/mainRoutes.js
+++ routing/mainRoutes.js
@@ -5,29 +5,59 @@ const User = require("../models/user"); //Imports user schmea we defined in the 
 
 
 
-routes.get('/', (req, res) => {
+routes.get('/register', (req, res) => {
     console.log("get request")
     res.render("register");
 })
 
-routes.post('/', (req, res) => {
+routes.post('/register', (req, res) => {
+    let validationPassed = true;
     console.log("post request");
-    const { email, password } = req.body; //Breaks down object properties into variables
-    bcrypt.genSalt(10, (err, salt) =>  {
-        if (err){
-            throw err;
-        }else{
-            bcrypt.hash(password, salt, (err, hash) => {
-                const newUser = new User({
-                    email: email,
-                    password: hash
+    const { email, name, password1, password2 } = req.body; //Breaks down object properties into variables
+    const errorMessages = new Array;
+    if (!name || !password1 || !password2 || !email){
+        validationPassed = false;
+        errorMessages.push("Not all fields are filled In");
+    }
+    if(password1 !== password2){
+        validationPassed = false;
+        errorMessages.push("Your passwords do not match");
+    }
+    if(password1.length < 5){
+        validationPassed = false;
+        errorMessages.push("Your password is too short");
+    }
+
+
+    if (validationPassed){  
+        bcrypt.genSalt(10, (err, salt) =>  {
+            if (err){
+                throw err;
+            }else{
+                bcrypt.hash(password1, salt, (err, hash) => {
+                    if (err){
+                        throw err; 
+                    }
+                    else{
+                        const newUser = new User({
+                            name: name,
+                            email: email,
+                            password: hash
+                        })
+                        newUser.save()
+                        .then(user => {
+                            console.log("User successfully saved to database")
+                            res.send("registered")
+                        })
+                        .catch(err => console.log(err.message))
+                    }
                 })
-                newUser.save()
-                .then(user => console.log("User successfully saved to database"))
-                .catch(err => console.log("An error occurred saving the user to the database"))
-            })
-        }
-    })
+            }
+        })
+    }
+    else{
+        res.send(errorMessages);
+    }
 })
 
 module.exports = routes;
